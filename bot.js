a9613a8a96e903573719d27d41859d92031bdc78
amarhaqeem13 <41633731+amarhaqeem13@users.noreply.github.com>
2018-07-26 17:47:23 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,36 +1,15 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+client.on('ready', () => {
+    console.log('I am ready!');
 });
 
-bot.on('message', function (user, userID, channelID, message, evt) {
-    
-    if (message.substring(0, 1) == 'z!') {
-        var args = message.substring(1).split(' ');
-        var cmd = args[0];
-       
-        args = args.splice(1);
-        switch(cmd) {
-            // z!ping
-            case 'ping':
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'Pong!'
-                });
-            break;
-                
-             args = args.splice(1);
-        switch(cmd) {
-            // z!help
-            case 'ping':
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'z!amar'
-                    message: 'z!zore'
-                });
-            break;
-         }
-     }
-});   
-    // THIS  MUST  BE  THIS  WAY
+client.on('message', message => {
+    if (message.content === 'z!ping') {
+    	message.reply('Pong!');
+  	}
+});
+
+// THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
